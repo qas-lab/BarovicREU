7406708803f5cfe3a0a1e74cccb98e0d50532afe
abarovi <abarovi2@uccs.edu>
2024-06-20 11:00:01 -0600

    Comments Added to files

--- Arduino Basic Testing/Test 4 (Two-way Bluetooth, NO ML)/bluetoothATest/bluetoothATest.ino
+++ Arduino Basic Testing/Test 4 (Two-way Bluetooth, NO ML)/bluetoothATest/bluetoothATest.ino
@@ -3,7 +3,7 @@
 
   This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE Sense 
   as a central device and looks for a specified service and characteristic in a 
-  peripheral device. If the specified service and characteristic is found in a 
+  peripheral device. If the specified service and characteristic are found in a 
   peripheral device, the last detected value of the on-board gesture sensor of 
   the Nano 33 BLE Sense, the APDS9960, is written in the specified characteristic. 
 
@@ -13,51 +13,55 @@
   This example code is in the public domain.
 */
 
-#include <ArduinoBLE.h>
-#include <Arduino_APDS9960.h>
+#include <ArduinoBLE.h>       // Include the ArduinoBLE library
+#include <Arduino_APDS9960.h> // Include the Arduino library for the APDS9960 sensor
 
+// UUIDs for the service and characteristic we want to interact with
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 
+// Variables to store the current and previous gesture values
 int gesture = -1;
 int oldGestureValue = -1;   
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  
+  Serial.begin(9600);       // Start the serial communication
+  while (!Serial);          // Wait for the serial port to connect
+
+  // Initialize the APDS9960 sensor
   if (!APDS.begin()) {
     Serial.println("* Error initializing APDS9960 sensor!");
-  } 
+  }
 
-  APDS.setGestureSensitivity(80); 
-  
+  APDS.setGestureSensitivity(80); // Set the sensitivity of the gesture sensor
+
+  // Initialize the BLE module
   if (!BLE.begin()) {
     Serial.println("* Starting Bluetooth® Low Energy module failed!");
-    while (1);
+    while (1);              // Halt the program if BLE initialization fails
   }
   
-  BLE.setLocalName("Nano 33 BLE (Central)"); 
-  BLE.advertise();
+  BLE.setLocalName("Nano 33 BLE (Central)"); // Set the local name for the BLE device
+  BLE.advertise();          // Start advertising the BLE device
 
   Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
   Serial.println(" ");
 }
 
 void loop() {
-  connectToPeripheral();
+  connectToPeripheral();    // Continuously try to connect to the peripheral device
 }
 
-void connectToPeripheral(){
+void connectToPeripheral() {
   BLEDevice peripheral;
   
   Serial.println("- Discovering peripheral device...");
 
-  do
-  {
+  // Scan for peripherals with the specified service UUID
+  do {
     BLE.scanForUuid(deviceServiceUuid);
     peripheral = BLE.available();
-  } while (!peripheral);
+  } while (!peripheral);    // Keep scanning until a peripheral is found
   
   if (peripheral) {
     Serial.println("* Peripheral device found!");
@@ -68,36 +72,40 @@ void connectToPeripheral(){
     Serial.print("* Advertised service UUID: ");
     Serial.println(peripheral.advertisedServiceUuid());
     Serial.println(" ");
-    BLE.stopScan();
-    controlPeripheral(peripheral);
+    BLE.stopScan();         // Stop scanning once the peripheral is found
+    controlPeripheral(peripheral); // Control the found peripheral
   }
 }
 
 void controlPeripheral(BLEDevice peripheral) {
   Serial.println("- Connecting to peripheral device...");
 
+  // Attempt to connect to the peripheral
   if (peripheral.connect()) {
     Serial.println("* Connected to peripheral device!");
     Serial.println(" ");
   } else {
     Serial.println("* Connection to peripheral device failed!");
     Serial.println(" ");
-    return;
+    return; // Exit the function if connection fails
   }
 
   Serial.println("- Discovering peripheral device attributes...");
+  // Attempt to discover the peripheral's attributes
   if (peripheral.discoverAttributes()) {
     Serial.println("* Peripheral device attributes discovered!");
     Serial.println(" ");
   } else {
     Serial.println("* Peripheral device attributes discovery failed!");
     Serial.println(" ");
-    peripheral.disconnect();
+    peripheral.disconnect(); // Disconnect if attribute discovery fails
     return;
   }
 
+  // Get the characteristic we want to write to
   BLECharacteristic gestureCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
-    
+  
+  // Check if the characteristic is found and writable
   if (!gestureCharacteristic) {
     Serial.println("* Peripheral device does not have gesture_type characteristic!");
     peripheral.disconnect();
@@ -108,9 +116,11 @@ void controlPeripheral(BLEDevice peripheral) {
     return;
   }
   
+  // Continuously check for gestures while connected to the peripheral
   while (peripheral.connected()) {
     gesture = gestureDetectection();
 
+    // Write the new gesture value if it has changed
     if (oldGestureValue != gesture) {  
       oldGestureValue = gesture;
       Serial.print("* Writing value to gesture_type characteristic: ");
@@ -119,15 +129,16 @@ void controlPeripheral(BLEDevice peripheral) {
       Serial.println("* Writing value to gesture_type characteristic done!");
       Serial.println(" ");
     }
-  
   }
   Serial.println("- Peripheral device disconnected!");
 }
-  
+
 int gestureDetectection() {
+  // Check if a gesture is available from the sensor
   if (APDS.gestureAvailable()) {
     gesture = APDS.readGesture();
 
+    // Print the detected gesture to the serial monitor
     switch (gesture) {
       case GESTURE_UP:
         Serial.println("- UP gesture detected");
@@ -144,7 +155,7 @@ int gestureDetectection() {
       default:
         Serial.println("- No gesture detected");
         break;
-      }
     }
-    return gesture;
+  }
+  return gesture; // Return the detected gesture
 }
--- Arduino Basic Testing/Test 4 (Two-way Bluetooth, NO ML)/bluetoothBTest/bluetoothBTest.ino
+++ Arduino Basic Testing/Test 4 (Two-way Bluetooth, NO ML)/bluetoothBTest/bluetoothBTest.ino
@@ -3,7 +3,7 @@
 
   This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE 
   as a peripheral device and specifies a service and a characteristic. Depending 
-  of the value of the specified characteristic, an on-board LED gets on. 
+  on the value of the specified characteristic, an on-board LED turns on.
 
   The circuit:
   - Arduino Nano 33 BLE. 
@@ -11,8 +11,9 @@
   This example code is in the public domain.
 */
 
-#include <ArduinoBLE.h>
-      
+#include <ArduinoBLE.h> // Include the ArduinoBLE library
+
+// Define gesture values using an enumeration for better readability
 enum {
   GESTURE_NONE  = -1,
   GESTURE_UP    = 0,
@@ -21,109 +22,115 @@ enum {
   GESTURE_RIGHT = 3
 };
 
+// UUIDs for the service and characteristic
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 
+// Variable to store the gesture value
 int gesture = -1;
 
+// Create a BLE service and characteristic
 BLEService gestureService(deviceServiceUuid); 
 BLEByteCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
 
-
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);  
+  Serial.begin(9600); // Start serial communication
+  while (!Serial);    // Wait for the serial port to connect
   
+  // Set the pin modes for the on-board LEDs
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   
+  // Initialize the LEDs to off state
   digitalWrite(LEDR, HIGH);
   digitalWrite(LEDG, HIGH);
   digitalWrite(LEDB, HIGH);
   digitalWrite(LED_BUILTIN, LOW);
 
-  
+  // Initialize the BLE module
   if (!BLE.begin()) {
     Serial.println("- Starting Bluetooth® Low Energy module failed!");
-    while (1);
+    while (1); // Halt the program if BLE initialization fails
   }
 
-  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
-  BLE.setAdvertisedService(gestureService);
-  gestureService.addCharacteristic(gestureCharacteristic);
-  BLE.addService(gestureService);
-  gestureCharacteristic.writeValue(-1);
-  BLE.advertise();
+  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)"); // Set the local name for the BLE device
+  BLE.setAdvertisedService(gestureService); // Advertise the service
+  gestureService.addCharacteristic(gestureCharacteristic); // Add the characteristic to the service
+  BLE.addService(gestureService); // Add the service
+  gestureCharacteristic.writeValue(-1); // Initialize the characteristic value
+  BLE.advertise(); // Start advertising the BLE device
 
   Serial.println("Nano 33 BLE (Peripheral Device)");
   Serial.println(" ");
 }
 
 void loop() {
-  BLEDevice central = BLE.central();
+  BLEDevice central = BLE.central(); // Wait for a central device to connect
   Serial.println("- Discovering central device...");
   delay(500);
 
-  if (central) {
+  if (central) { // If a central device is found
     Serial.println("* Connected to central device!");
     Serial.print("* Device MAC address: ");
     Serial.println(central.address());
     Serial.println(" ");
 
+    // While the central device is connected
     while (central.connected()) {
-      if (gestureCharacteristic.written()) {
-         gesture = gestureCharacteristic.value();
-         writeGesture(gesture);
-       }
+      if (gestureCharacteristic.written()) { // If the characteristic value is written
+        gesture = gestureCharacteristic.value(); // Get the new gesture value
+        writeGesture(gesture); // Perform the action based on the gesture
+      }
     }
     
-    Serial.println("* Disconnected to central device!");
+    Serial.println("* Disconnected from central device!");
   }
 }
 
+// Function to perform actions based on the gesture value
 void writeGesture(int gesture) {
   Serial.println("- Characteristic <gesture_type> has changed!");
   
-   switch (gesture) {
-      case GESTURE_UP:
-        Serial.println("* Actual value: UP (red LED on)");
-        Serial.println(" ");
-        digitalWrite(LEDR, LOW);
-        digitalWrite(LEDG, HIGH);
-        digitalWrite(LEDB, HIGH);
-        digitalWrite(LED_BUILTIN, LOW);
-        break;
-      case GESTURE_DOWN:
-        Serial.println("* Actual value: DOWN (green LED on)");
-        Serial.println(" ");
-        digitalWrite(LEDR, HIGH);
-        digitalWrite(LEDG, LOW);
-        digitalWrite(LEDB, HIGH);
-        digitalWrite(LED_BUILTIN, LOW);
-        break;
-      case GESTURE_LEFT:
-        Serial.println("* Actual value: LEFT (blue LED on)");
-        Serial.println(" ");
-        digitalWrite(LEDR, HIGH);
-        digitalWrite(LEDG, HIGH);
-        digitalWrite(LEDB, LOW);
-        digitalWrite(LED_BUILTIN, LOW);
-        break;
-      case GESTURE_RIGHT:
-        Serial.println("* Actual value: RIGHT (built-in LED on)");
-        Serial.println(" ");
-        digitalWrite(LEDR, HIGH);
-        digitalWrite(LEDG, HIGH);
-        digitalWrite(LEDB, HIGH);
-        digitalWrite(LED_BUILTIN, HIGH);
-        break;
-      default:
-        digitalWrite(LEDR, HIGH);
-        digitalWrite(LEDG, HIGH);
-        digitalWrite(LEDB, HIGH);
-        digitalWrite(LED_BUILTIN, LOW);
-        break;
-    }      
+  switch (gesture) {
+    case GESTURE_UP:
+      Serial.println("* Actual value: UP (red LED on)");
+      Serial.println(" ");
+      digitalWrite(LEDR, LOW); // Turn on the red LED
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, HIGH);
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+    case GESTURE_DOWN:
+      Serial.println("* Actual value: DOWN (green LED on)");
+      Serial.println(" ");
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, LOW); // Turn on the green LED
+      digitalWrite(LEDB, HIGH);
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+    case GESTURE_LEFT:
+      Serial.println("* Actual value: LEFT (blue LED on)");
+      Serial.println(" ");
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, LOW); // Turn on the blue LED
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+    case GESTURE_RIGHT:
+      Serial.println("* Actual value: RIGHT (built-in LED on)");
+      Serial.println(" ");
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+      break;
+    default:
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, HIGH);
+      digitalWrite(LED_BUILTIN, LOW); // Turn off all LEDs for undefined gestures
+      break;
+  }      
 }
