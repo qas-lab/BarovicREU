02f41ae0c1ca77188dfa144773334eb535fa7b0e
abarovi <abarovi2@uccs.edu>
2024-07-24 10:21:11 -0600

    Update modified_color_command_keyword_microphone_continuous.ino

--- Arduino Final Model (1-Chip)/modified_color_command_keyword_microphone_continuous/modified_color_command_keyword_microphone_continuous.ino
+++ Arduino Final Model (1-Chip)/modified_color_command_keyword_microphone_continuous/modified_color_command_keyword_microphone_continuous.ino
@@ -163,6 +163,7 @@ void loop()
         else if(wakeUp == true)
         {
           //Start Chip Logic
+          toggleFlags(predict);
           
         }
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
@@ -172,7 +173,81 @@ void loop()
         print_results = 0;
     }
 }
-
+void toggleFlags(int predict)
+{
+  switch (predict) 
+          {
+            case 0:
+              
+            break;
+            case 1:
+              
+            break;
+            case 2:
+              
+            break;
+            case 3:
+              
+            break;
+            case 4:
+              
+            break;
+            case 5:
+              
+            break;
+            case 6:
+              
+            break;
+            case 7:
+              
+            break;
+            case 8:
+              
+            break;
+            case 9:
+              
+            break;
+            case 10:
+              
+            break;
+            case 11:
+              
+            break;
+            case 12:
+              
+            break;
+            case 13:
+              
+            break;
+            case 14:
+              
+            break;
+            case 15:
+              
+            break;
+            case 16:
+              
+            break;
+            case 17:
+              
+            break;
+            case 18:
+              
+            break;
+            case 19:
+              
+            break;
+            case 20:
+              
+            break;
+            case 21:
+              
+            break;
+            case 22:
+              
+            break;
+          }
+}
 int toggleListenMode()
 {
   if(listenMode == false)
